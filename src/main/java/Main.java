88db1be53124c8669a9abad87cb4b5e97a24fb8d
folicks <139702088+folicks@users.noreply.github.com>
2024-08-07 14:43:40 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void fizzBuzz(int n) {
+    for (int i = 1; i <= n; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)){
+            System.out.println("FizzBuzz");
+        } else if (i % 5 == 0) {
+            System.out.println("Buzz");
+        }else if (i % 3 == 0) {
+            System.out.println("Fizz");
+        } else {
+            System.out.println(i);
+        }
+    }
+  }
+
+  public static void main(String[] args) {
+    fizzBuzz(100);
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
+
+
